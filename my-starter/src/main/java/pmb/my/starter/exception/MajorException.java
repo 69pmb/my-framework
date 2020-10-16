82f7f76ab3d47deb4d35c530dbe976de78a50707
pmb <pm.broca@gmail.com>
2020-10-16 22:25:20 +0200

    feat: exceptions & utils

---
+++ my-starter/src/main/java/pmb/my/starter/exception/MajorException.java
@@ -0,0 +1,35 @@
+package pmb.my.starter.exception;
+
+/**
+ * Checked exception of the application, for usual or big exceptions.
+ *
+ */
+public class MajorException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Constructeur avec message.
+     *
+     * @param message le message d'erreur
+     */
+    public MajorException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructeur avec message et throwable.
+     *
+     * @param message le message d'erreur
+     * @param throwable la cause de l'erreur
+     */
+    public MajorException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+
+    /**
+     * Constructeur parent vide.
+     */
+    public MajorException() {
+        super();
+    }
+}
---
+++ my-starter/src/main/java/pmb/my/starter/utils/MyConstant.java
@@ -0,0 +1,80 @@
+package pmb.my.starter.utils;
+
+import java.nio.charset.Charset;
+import java.text.DecimalFormat;
+import java.util.regex.Pattern;
+
+/**
+ * Constants class.
+ *
+ */
+public final class MyConstant {
+
+    /**
+     * File separator.
+     */
+    public static final String FS = System.getProperty("file.separator");
+
+    /**
+     * User directory, launching location of the app.
+     */
+    public static final String USER_DIRECTORY = System.getProperty("user.dir") + FS;
+
+    /**
+     * Chemin des resources de l'application.
+     */
+    public static final String RESOURCES_DIRECTORY = USER_DIRECTORY + "src" + FS + "main" + FS
+            + "resources" + FS;
+
+    /**
+     * Chemin abs du fichier de log.
+     */
+    public static final String FILE_LOG_PATH = USER_DIRECTORY + "error.log";
+
+    public static final String CONFIGURATION_FILENAME = "config.properties";
+
+    public static final String NEW_LINE = "\r\n";
+
+    public static final String ANSI_ENCODING = "Cp1252";
+
+    public static final Charset ANSI_CHARSET = Charset.forName(MyConstant.ANSI_ENCODING);
+
+    public static final String DOT = ".";
+
+    public static final String QUOTE = "\"";
+
+    public static final String PACKAGE_NAME = "pmb";
+
+    public static final String XML_EXTENSION = ".xml";
+
+    public static final String CSV_EXTENSION = ".csv";
+
+    public static final String TXT_EXTENSION = ".txt";
+
+    private static final String[] FORBIDDEN_CHARACTERS_FILENAME = { "<", ">", ":", "\"", "/", "|", "*", "?" };
+
+    private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#0.00");
+
+    public static final String REGEX_PUNCTUATION = "\\p{Punct}|\\s";
+
+    public static final Pattern PATTERN_PUNCTUATION = Pattern.compile(REGEX_PUNCTUATION);
+
+    private MyConstant() {
+        throw new AssertionError("Must not be used");
+    }
+
+    /**
+     * Chemin abs du fichier de configuration.
+     */
+    public static String getConfigPath() {
+        return RESOURCES_DIRECTORY + CONFIGURATION_FILENAME;
+    }
+
+    public static String[] getForbiddenCharactersFilename() {
+        return FORBIDDEN_CHARACTERS_FILENAME;
+    }
+
+    public static DecimalFormat getDecimalFormat() {
+        return DECIMAL_FORMAT;
+    }
+}
